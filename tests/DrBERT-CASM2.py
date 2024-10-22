7a735166749c7e210921452ae02204d1b33a23ba
Dring0 <161295622+Dring0@users.noreply.github.com>
2024-10-22 11:55:29 +0200

    amélioration negation detector

--- tests/DrBERT-CASM2.py
+++ tests/DrBERT-CASM2.py
@@ -6,6 +6,7 @@ matcher = HFEntityMatcher(model="medkit/DrBERT-CASM2")
 test_doc = TextDocument("Il ne boit pas d'alcool et ne fume pas de tabac.")
 detected_entities = matcher.run([test_doc.raw_segment])
 
+
 # show information
 msg = "|".join(f"'{entity.label}':{entity.text}" for entity in detected_entities)
 print(f"Text: '{test_doc.text}'\n{msg}")
--- tests/Negation_detector.py
+++ tests/Negation_detector.py
@@ -1,31 +1,78 @@
+#Tuto:
+#https://medkit-lib.org/user_guide/first_steps.html
+
 from pathlib import Path
 from medkit.core.text import TextDocument
 from medkit.text.segmentation import SentenceTokenizer
+from medkit.text.segmentation import SyntagmaTokenizer
+from medkit.text.ner.hf_entity_matcher import HFEntityMatcher
 from medkit.text.context import NegationDetector, NegationDetectorRule
+from medkit.text.ner import RegexpMatcher, RegexpMatcherRule
 
 #importation du texte
 doc = TextDocument.from_file(Path(r"C:\Users\Administrateur\Documents\ENSTA Bretagne\2A\aime\tests\ENSTAP1.txt"))
 
-#tokenisation du texte en phrases
+#initialisation du tokenizer de phrases
 sent_tokenizer = SentenceTokenizer(
     output_label="sentence",
     punct_chars=[".", "?", "!"],
 )
 
+#segmentation du texte en phrases
 sentences = sent_tokenizer.run([doc.raw_segment])
 
+# #affichage des phrases
 # for sentence in sentences:
 #     print(f"uid={sentence.uid}")
 #     print(f"text={sentence.text!r}")
 #     print(f"spans={sentence.spans}, label={sentence.label}\n")
 
-#detection des negations
+#initialisation du tokenizer de syntagmes
+synt_tokenizer = SyntagmaTokenizer(
+    output_label="syntagma",
+    separators=[r"\bmais\b", r"\bet\b"],
+)
 
-negation_detector = NegationDetector(output_label="negation")
-negation_detector.run(syntagma_segs)
+#segmentation des phrases en syntagmes
+syntagmas = synt_tokenizer.run(sentences)
 
-# Display negated syntagmas
-for syntagma_seg in syntagma_segs:
-    negation_attr = syntagma_seg.attrs.get(label="negation")[0]
-    if negation_attr.value:
-        print(syntagma_seg.text)
+
+
+
+
+#Détection des négations
+
+#initialisation des règles de détection
+neg_rules = [
+    NegationDetectorRule(regexp=r"\bpas\s*d[' e]\b"),
+    NegationDetectorRule(regexp=r"\bsans\b", exclusion_regexps=[r"\bsans\s*doute\b"]),
+    NegationDetectorRule(regexp=r"\bne\s*semble\s*pas"),
+]
+#initialisation du détecteur de négations
+neg_detector = NegationDetector(output_label="is_negated", rules=neg_rules)
+#détection des négations
+neg_detector.run(syntagmas) #ne renvoie rien, ajoute un booléan à chaque syntagme pour indiquer si une négation a été détectée ou non
+
+# #affichage des syntagmes avec négation
+# for syntagma in syntagmas:
+#     neg_attr = syntagma.attrs.get(label="is_negated")[0]
+#     if neg_attr.value:
+#         print(syntagma.text)
+
+#Entity Matcher
+
+#initialisation du matcher
+matcher = HFEntityMatcher(model="medkit/DrBERT-CASM2", attrs_to_copy=["is_negated"])
+
+#detection des entités
+entities = matcher.run(syntagmas)
+
+for entity in entities:
+    neg_attr = entity.attrs.get(label="is_negated")[0]
+    if not neg_attr.value and entity.label == "problem":
+        print(f"text='{entity.text}', label={entity.label}, is_negated={neg_attr.value}")
+    #print(f"text='{entity.text}', label={entity.label}, is_negated={neg_attr.value}")
+
+# #affichage des entités
+# msg = "|".join(f"'{entity.label}':{entity.text}" for entity in detected_entities)
+# print(msg)
